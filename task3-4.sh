9f6f00b306cf878b32c64f9d6fd9a118d7ee2169
sastsy <sastsybina@gmail.com>
2022-11-24 18:35:07 +0300

    added tr command

--- task3-4.sh
+++ task3-4.sh
@@ -2,7 +2,8 @@
 
 file="renamer.sh"
 touch "$file"
-echo  "#!/bin/bash" > "$file"
-echo 'for i in *; do mv "$i" "${i^^}"; done' >> "$file" 
+#new="$(echo "$file" | tr '[a-z]' '[A-Z]')"
+echo  "#!/bin/bash" >> "$file"
+echo 'for i in *; do mv "$i" "$(echo "$i" | tr '[a-z]' '[A-Z]')"; done' >> "$file" 
 
 
