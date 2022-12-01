eaa2ab0c549e308e9bfa1f75f7a0d330a44e47c6
sastsy <53853716+sastsy@users.noreply.github.com>
2022-12-01 13:55:20 +0300

    Update task3-1.sh

--- task3-1.sh
+++ task3-1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 substr=$(echo $(file -b --mime-type $1) | cut -d$'/' -f 1)
 case "$substr" in
-"text" ) open -a "LibreOffice Writer" $1;;
-"image" ) open $1;;
-"video" ) open $1;;
+"text" ) libreoffice $1;;
+"image" ) gimp $1;;
+"video" ) mpv $1;;
 *) echo "i cannot recognise the file type";;
 esac
-#didnt find the command to open file in a specific application
